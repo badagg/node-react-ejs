2645cbd392138d9fee749ec1023a76f9250d97ba
Z.Key <zkey@zhoubingdeMacBook-Pro.local>
2016-06-01 09:16:16 +0800

    node-react-ejs
    React服务端渲染

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var  JSX = require('node-jsx').install();
+var express = require('express');
+var router = express.Router();
+var React = require('react');
+var ReactDom = require('react-dom/server');
+var Hello = React.createFactory(require("../components/Hello.jsx"));
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	var reactHtml = ReactDom.renderToString(Hello()); //这一步报错
+	res.render('index', { html:reactHtml });
+});
+
+module.exports = router;
